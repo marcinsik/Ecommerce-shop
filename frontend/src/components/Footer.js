51942f32c262d048237eb6b93e2578b39c438fc1
Marcin Sikorski <sikorskimarcin1@gmail.com>
2023-04-15 21:45:31 +0200

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+function Footer() {
+  return (
+    <div>
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-4'>Copyright &copy; Marcin Sikorski</Col>
+                </Row>
+            </Container>
+
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Nav, Navbar, Container, Row,} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+        <header>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                    <   Navbar.Brand>MyShop</Navbar.Brand>
+                    </LinkContainer>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+
+                        <LinkContainer to ='/koszyk'>
+                            <Nav.Link><i className = "fas fa-shopping-cart"></i> Koszyk</Nav.Link>
+                        </LinkContainer>
+                        
+                        <LinkContainer to = '/logowanie'>
+                            <Nav.Link><i className = "fas fa-user"></i> Logowanie</Nav.Link>
+                        </LinkContainer>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+  )
+}
+
+export default Header
